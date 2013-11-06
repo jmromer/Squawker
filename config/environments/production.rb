204d63e744fe52a680edac57ca58a60b93f356cd
Jake Romer <jacob.m.romer@gmail.com>
2013-11-06 10:14:36 -0500

    adds sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,13 @@ Squawker::Application.configure do
 
   # Base URL for links in mailers
   config.action_mailer.default_url_options = { host: "squawker.jakeromer.com" }
+
+  ActionMailer::Base.smtp_settings = {
+    address:         "smtp.sendgrid.net",
+    port:            "25",
+    authentication:  :plain,
+    user_name:       ENV['SENDGRID_USERNAME'],
+    password:        ENV['SENDGRID_PASSWORD'],
+    domain:          ENV['SENDGRID_DOMAIN']
+  }
 end
