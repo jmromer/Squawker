63128c526000bd4bc12e618d0436c5509ba2538f
Jake Romer <jacob.m.romer@gmail.com>
2013-11-14 11:26:29 -0500

    fixes views for safari

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ActionMailer::Base
-  default from: "no-reply@squawker.jakeromer.com"
+  default from: "no-reply@squawknation.net"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.password_reset.subject
-  #
+
   def password_reset(user)
     @user = user
     mail to: user.email, subject: "Reset your Squawker password."
