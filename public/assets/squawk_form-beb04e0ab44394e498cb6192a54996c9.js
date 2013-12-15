b0c63cef913888f6f28780a6e057110017df3a57
Jake Romer <jacob.m.romer@gmail.com>
2013-12-14 20:26:02 -0500

    precompile

---
+++ public/assets/squawk_form-beb04e0ab44394e498cb6192a54996c9.js
@@ -0,0 +1,1 @@
+function updateCountdown(){var n=$("#squawk_content").val().length,t=160-n;160===t?$("#countdown").text(""):10>=t?$("#countdown").text(t).css("color","red"):$("#countdown").text(t).css("color","black")}$(document).ready(function(){updateCountdown(),$("#squawk_content").change(updateCountdown),$("#squawk_content").keyup(updateCountdown)});
