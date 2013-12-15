a69c724329114dc90140e886d09fe6124eb83a67
Jake Romer <jacob.m.romer@gmail.com>
2013-12-15 01:49:10 -0500

    adds title to delete links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,16 @@ module ApplicationHelper
 
   def full_title(page_title)
     base_title = "Squawker"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
+    page_title.empty? ? base_title : "#{base_title} | #{page_title}"
   end
 
   def trash_can_icon
     trash_icon_url = "http://s3.amazonaws.com/squawker/icon_trash.gif"
-    image_tag(trash_icon_url, border: 0,
-      alt: 'Delete', title: 'Delete', class: 'trashcan')
+    image_tag( trash_icon_url,
+                border: 0,
+                alt: 'Delete',
+                title: 'Delete',
+                class: 'trashcan' )
   end
 
 
