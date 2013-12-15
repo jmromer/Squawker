c294fa4b0ada5b1f96e900abb89d560bc78e9f56
Jake Romer <jacob.m.romer@gmail.com>
2013-12-14 20:12:39 -0500

    hides countdown when equal to 160

--- app/assets/javascripts/squawk_form.js
+++ app/assets/javascripts/squawk_form.js
@@ -2,7 +2,9 @@ function updateCountdown () {
   var currentChars   = $('#squawk_content').val().length
   var remainingChars = 160 - currentChars;
 
-  if (remainingChars <= 10) {
+  if (remainingChars === 160 ) {
+    $('#countdown').text('');
+  } else if (remainingChars <= 10) {
     $('#countdown').text(remainingChars).css('color', 'red');
   } else {
     $('#countdown').text(remainingChars).css('color', 'black');
