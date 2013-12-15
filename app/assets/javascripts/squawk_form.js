265152e3deabcf9ca9006d22f66398833a40121c
Jake Romer <jacob.m.romer@gmail.com>
2013-12-15 02:00:36 -0500

    polishes countdown: now appears on focus and disappears on focus lost

--- app/assets/javascripts/squawk_form.js
+++ app/assets/javascripts/squawk_form.js
@@ -2,9 +2,7 @@ function updateCountdown () {
   var current   = $('#squawk_content').val().length
   var remaining = 160 - current;
 
-  if (remaining === 160 ) {
-    $('#countdown').html('');
-  } else if (remaining <= 10) {
+  if (remaining <= 10) {
     $('#countdown').text(remaining).css('color', 'red');
   } else {
     $('#countdown').text(remaining).css('color', 'black');
@@ -12,10 +10,15 @@ function updateCountdown () {
 }
 
 $(document).ready(function(){
-  if ($('#squawk_content').val() != undefined) {
+  $('#squawk_content').focus(function(){
     updateCountdown();
     $('#squawk_content').change(updateCountdown);
     $('#squawk_content').keyup(updateCountdown);
-  }
+  });
 });
 
+$(document).ready(function(){
+  $('#squawk_content').focusout(function(){
+    $('#countdown').text('');
+  });
+});
