b0a4f497c150602d21015b1ab92e22cdd0bc097e
Jake Romer <jkrmr@users.noreply.github.com>
2014-11-29 23:58:20 -0800

    Update images location

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
   end
 
   def trash_can_icon
-    trash_icon_url = "http://s3.amazonaws.com/squawker/icon_trash.gif"
+    trash_icon_url = "/public/assets/images/icon_trash.gif"
     image_tag( trash_icon_url,
                 border: 0,
                 alt: 'Delete',
