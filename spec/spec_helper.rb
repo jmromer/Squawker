88092ce87622046f562cd8108d86e1cc7ea8a17b
Jake Romer <jkrmr@users.noreply.github.com>
2015-03-08 00:38:07 -0800

    Remove spork preloading

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,44 +1,35 @@
 require 'rubygems'
-require 'spork'
 
-Spork.prefork do
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
-  require 'rspec/autorun'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
 
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-  # Checks for pending migrations before tests are run.
-  ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+# Checks for pending migrations before tests are run.
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
-  RSpec.configure do |config|
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
-    config.use_transactional_fixtures = true
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
 
-    # for targeted testing
-    config.treat_symbols_as_metadata_keys_with_true_values = true
-    config.filter_run focus: true
-    config.run_all_when_everything_filtered = true
+  # for targeted testing
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 
-    config.infer_base_class_for_anonymous_controllers = false
-    config.order = "random"
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = 'random'
 
-    # Include the Capybara DSL so that specs in spec/requests still work.
-    config.include Capybara::DSL
+  # Include the Capybara DSL so that specs in spec/requests still work.
+  config.include Capybara::DSL
 
-    # Disable the old-style object.should syntax.
-    config.expect_with :rspec do |c|
-      c.syntax = :expect
-    end
-  end # RSpec
-
+  # Disable the old-style object.should syntax.
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
 
-# add factorygirl to pick up changes from factories
-Spork.each_run do
-  # This code will be run each time you run your specs.
-  FactoryGirl.reload
-end
