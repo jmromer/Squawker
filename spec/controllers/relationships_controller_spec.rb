552bdeefc2007240f46be1fc1aeed84e822e4223
Jake Romer <jkrmr@users.noreply.github.com>
2015-05-14 02:00:18 -0700

    Linting

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -1,30 +1,28 @@
 require 'spec_helper'
 
 describe RelationshipsController do
-
   let(:user) { FactoryGirl.create(:user) }
   let(:other_user) { FactoryGirl.create(:user) }
 
   before { sign_in user, no_capybara: true }
 
-  describe "creating a relationship with Ajax" do
-
+  describe 'creating a relationship with Ajax' do
     it "should increment the Relationship count" do
       expect do
         xhr :post, :create, relationship: { followed_id: other_user.id }
       end.to change(Relationship, :count).by(1)
     end
 
-    it "should respond with success" do
+    it "should respond with su ccess" do
       xhr :post, :create, relationship: { followed_id: other_user.id }
       expect(response).to be_success
     end
   end
 
-  describe "destroying a relationship with Ajax" do
+  describe 'destroying a relationship with Ajax' do
+    let(:relationship) { user.relationships.find_by(followed_id: other_user) }
 
     before { user.follow!(other_user) }
-    let(:relationship) { user.relationships.find_by(followed_id: other_user) }
 
     it "should decrement the Relationship count" do
       expect do
@@ -32,7 +30,7 @@ describe RelationshipsController do
       end.to change(Relationship, :count).by(-1)
     end
 
-    it "should respond with success" do
+    it 'should respond with success' do
       xhr :delete, :destroy, id: relationship.id
       expect(response).to be_success
     end
