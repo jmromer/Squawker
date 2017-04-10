a85ef51aa64468822c084af8015b7e30786c6fc6
Jake Romer <jkrmr.io@gmail.com>
2017-04-09 20:10:24 -0700

    Replace gif with svg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,11 @@ module ApplicationHelper
   end
 
   def trash_can_icon
-    image_tag("icon_trash.gif", border: 0, alt: "Delete",
-                                title: "Delete", class: "trashcan")
+    <<-HTML.html_safe
+      <svg class="trashcan" viewBox="0 0 12 16" version="1.1" aria-hidden="true">
+        <path fill-rule="evenodd" d="M11 2H9c0-.55-.45-1-1-1H5c-.55 0-1 .45-1 1H2c-.55 0-1 .45-1 1v1c0 .55.45 1 1 1v9c0 .55.45 1 1 1h7c.55 0 1-.45 1-1V5c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 12H3V5h1v8h1V5h1v8h1V5h1v8h1V5h1v9zm1-10H2V3h9v1z">
+        </path>
+      </svg>
+    HTML
   end
 end
