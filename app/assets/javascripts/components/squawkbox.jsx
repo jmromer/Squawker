76cb0626d4f50f7c2a69b924bb0725f824c507f3
Jake Romer <jkrmr.io@gmail.com>
2017-05-10 10:14:06 -0700

    Use React for countdown updates and submission

---
+++ app/assets/javascripts/components/squawkbox.jsx
@@ -0,0 +1,48 @@
+class SquawkBox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {remainingChars: "", color: "black" }
+    this.updateCountdown = this.updateCountdown.bind(this)
+    this.clearCountdown = this.clearCountdown.bind(this)
+  }
+
+  render() {
+    return <field>
+      <textarea cols="30"
+                placeholder="sing the song of yourself"
+                className="squawk-form"
+                name="squawk[content]"
+                onKeyDown={this.updateCountdown}
+                onFocus={this.updateCountdown}
+                onChange={this.updateCountdown}
+                onBlur={this.clearCountdown}>
+      </textarea>
+
+      <div className="squawk-form-countdown"
+           style={{color: this.state.color}}>
+        {this.state.remainingChars}
+      </div>
+  </field>
+  }
+
+  updateCountdown(event) {
+    if ((event.metaKey || event.ctrlKey) && event.keyCode == 13) {
+      document.getElementById("js-squawk-form").submit()
+    }
+
+    let maxLength = 160
+    let currentLength = event.target.value.length
+    let remaining = maxLength - currentLength
+    let color = (remaining <= 10) ? "red" : "black"
+
+    if (remaining === maxLength) {
+      this.setState({remainingChars: ""})
+    } else {
+      this.setState({remainingChars: remaining, color: color})
+    }
+  }
+
+  clearCountdown(event) {
+    this.setState({remainingChars: ""})
+  }
+}
