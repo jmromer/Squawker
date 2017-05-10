92c3e16727f00e063ab8fd5e7f1790661cc188c0
Jake Romer <jkrmr.io@gmail.com>
2017-05-09 20:33:55 -0700

    Kill the users index (#7)

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -70,11 +70,6 @@ describe "Authentication" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
         end
-
-        describe "visiting the user index" do
-          before { visit users_path }
-          it { should have_title("Sign In") }
-        end
       end # users controller
 
       describe "when attempting to visit a protected page" do
